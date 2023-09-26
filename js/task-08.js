b3c011f9210454ea85ee08d5b142dcc5c94e428e
Anhelinkaaa <bragaangelina94@gmail.com>
2023-09-26 19:35:43 +0300

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const sub = document.querySelector(".login-form");
+sub.addEventListener("submit", subForm);
 
+function subForm(evt) {
+  evt.preventDefault();
+  const form = evt.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Bсі поля повинні бути заповнені.");
+  } else {
+    const contacts = {
+      Email: email,
+      Password: password,
+    };
+    console.log(contacts);
+  }
+  form.reset();
+}
