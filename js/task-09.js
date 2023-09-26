756e5952e9abf48be98eb51e17a37fe0d2c3eca4
Anhelinkaaa <bragaangelina94@gmail.com>
2023-09-26 19:53:44 +0300

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body')
+const color = document.querySelector('.color')
+const button = document.querySelector('.change-color')
+
+button.addEventListener('click', changeColor)
+
+function changeColor(evt) {
+  const arr = getRandomHexColor()
+  
+  body.style.backgroundColor = arr;
+color.textContent = `${arr}`
+
+  
+  console.dir(color.textContent)
+  console.log(evt)
+  console.log(evt)
+}
